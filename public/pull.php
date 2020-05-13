be65149c76d0dfb2257b4883277a0ca7d260a007
thien <thien>
2020-05-13 17:59:58 +0700

    testing

--- public/pull.php
+++ public/pull.php
@@ -1,9 +1,6 @@
 <?php
 
 // Use in the “Post-Receive URLs” section of your GitHub repo.
-
-if ( $_POST['payload'] ) {
-    shell_exec( 'cd /var/www/html/laravel_blog/ && git reset –hard HEAD && git pull' );
-}
+shell_exec( 'cd /var/www/html/laravel_blog/ && git reset –hard HEAD && git pull' );
 
 ?>
